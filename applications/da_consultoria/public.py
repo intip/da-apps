3cb5fcc83cf8667277c5e199e497a192b78a752e
Arthur <arthur.4lves@gmail.com>
2013-09-18 11:02:09 -0300

    Adiciona aplicativo Consultoria Juridica

---
+++ applications/da_consultoria/public.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+#
+# Copyright 2009 Prima Tech Informatica Ltda.
+#
+# Licensed under the Environ License, Version 1.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.cmspublica.com.br/licenses/ENVIRON-LICENSE-1.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from publica import settings
+from publica.utils.json import encode, decode
+from publica.utils.decorators import dbconnectionapp, serialize, jsoncallback
+
+
+class Public(object):
+
+    """
+        public class of methods of this content
+    """
+    @dbconnectionapp
+    def _get_consultoria(self, limit=10, offset=0):
+        """
+        	Retorna do banco os dados cadastrados em consultoria juridica
+        """
+        lista = []
+        for i in self.execSql("select_consultoria_limit",
+                              limit=int(limit),
+                              offset=int(offset)):
+            lista.append(i)
+            
+        return {"itens":lista, "qtde":self.get_count()}
+
+    @jsoncallback
+    def get_consultoria(self, limit=10, offset=0):
+        return self._get_consultoria(limit=limit, offset=offset)
+
+
+    @dbconnectionapp
+    def get_count(self):
+        count = list(self.execSql("select_count_for_paginator"))[0]['count']
+        return count
+         
+        
+
+        
+
+        
+
+        
